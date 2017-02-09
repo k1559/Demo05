2c04c6552dd32599c09fdc81bd9fcd78c0443fe7
K1559 <K1559@student.jamk.fi>
2017-02-09 15:09:12 +0200

    tehtävä1

--- teht01/Program.cs
+++ teht01/Program.cs
@@ -10,6 +10,58 @@ namespace teht01
     {
         static void Main(string[] args)
         {
+            //create a few Person objects
+            Person person1 = new Person
+            {
+                Firstname = "Kirsi", Lastname="Mainio", SocialSecurityNumber = "051060-0000"
+            };
+
+            Person person2 = new Person
+            {
+                Firstname = "Mirtsu",
+                Lastname = "Hopo",
+                SocialSecurityNumber = "021030-0001"
+            };
+            Person person3 = new Person
+            {
+                Firstname = "Fernando",
+                Lastname = "Torres",
+                SocialSecurityNumber = "053020-0350"
+            };
+
+            // create a Persons object
+            Persons imaginaryfriends = new Persons();
+
+            //add person objects to persons collection
+            imaginaryfriends.AddPerson(person1);
+            imaginaryfriends.AddPerson(person2);
+            imaginaryfriends.AddPerson(person3);
+
+            // get one person from Persons collection
+            Person person4 = imaginaryfriends.GetPerson(2);
+            if(person4 != null)
+            {
+                Console.WriteLine(person4.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Person not found in that index!");
+            }
+
+            // print collection
+            imaginaryfriends.PrintCollection();
+
+            //find person
+            string sotu = "053020-0350";
+            Console.WriteLine("Find sotu: " + sotu);
+            Person person5 = imaginaryfriends.FindPerson(sotu);
+            if (person5 != null)
+            {
+                Console.WriteLine(person5.ToString());
+            }else
+            {
+                Console.WriteLine("Cant find person with that sotu..");
+            }
         }
     }
 }
